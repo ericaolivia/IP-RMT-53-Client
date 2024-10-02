25dcb563bc3447609eebadc5206796ad8746efbe
erica olivia <ecaoliviaaa@gmail.com>
2024-10-02 17:48:43 +0800

    chore:add hero & carousel in the homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,15 @@
-export default function Hero() {
+export default function Hero({targetRef}) {
+  const handleScroll = () => {
+    targetRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+        <div className="max-w-2xl">
+          <h2 className="text-5xl font-bold">Welcome to Sizzle & Stir! </h2>
+          <p className="py-6">Explore tremendous amount of beloved recipes that suit your tastes!</p>
+          <button className="btn btn-warning" onClick={handleScroll}>Explore Now</button>
         </div>
       </div>
     </div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,19 @@
 import Navbar from "../components/Navbar";
 import Carousel from "../components/Carousel";
 import Hero from "../components/Hero";
+import { useRef } from "react";
 
 export default function Homepage() {
+  const targetRef = useRef(null);
+
   return (
     <>
       <Navbar />
       <Carousel />
-      <Hero />
+      <Hero targetRef={targetRef}/>
+      <div ref={targetRef}>
+        <h1 className="w-full h-80">Explore Recipes</h1>
+      </div>
     </>
   );
 }
